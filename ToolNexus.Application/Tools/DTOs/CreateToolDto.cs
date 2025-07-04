a1c0f72dbe3cd2ced90ba7761119b66672de1a6c
LukaMolicnik <lm6069@student.uni-lj.si>
2025-07-04 11:32:49 +0200

    changed the frontend structure

---
+++ ToolNexus.Application/Tools/DTOs/CreateToolDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolNexus.Application.Tools.DTOs
+{
+    public class CreateToolDto
+    {
+        [Required(ErrorMessage = "Koda orodja je obvezna")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Code { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Ime je obvezno")]
+        [StringLength(50, ErrorMessage = "Ime ne sme biti daljše od 50 znakov")]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(100, ErrorMessage = "Opis ne sme biti daljši od 100 znakov")]
+        public string Description { get; set; } = string.Empty;
+
+        public string Version { get; set; }
+    }
+}
